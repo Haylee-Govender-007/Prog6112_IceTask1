83a24ee4428d0c7cd55111de8bfd70cb6a65b5c5
Haylee-Govender-007 <ST10480667@myemeris.edu.za>
2026-07-30 18:55:52 +0000

    4th commit
    Added functionality to capture student numbers and challenge marks

--- src/main/java/com/mycompany/icetask1_prog6112/IceTask1_Prog6112.java
+++ src/main/java/com/mycompany/icetask1_prog6112/IceTask1_Prog6112.java
@@ -25,5 +25,37 @@ public class IceTask1_Prog6112 {
         // two-dimensional array
         // Columns: 0 = Student Number, 1 = Challenge 1, 2 = Challenge 2, 3 = Challenge 3, 4 = Total Marks, 5 = Percentage
         double[][] results = new double[students][6];
+        
+        // Capture details for every student
+
+        for (int i = 0; i < results.length; i++) {
+
+            System.out.println("\nEnter details for student " + (i + 1));
+
+            // Read student number
+            System.out.print("Student number: ");
+            results[i][0] = kb.nextInt();
+
+            // Read Challenge 1 mark
+            System.out.print("Challenge 1 mark: ");
+            results[i][1] = kb.nextInt();
+
+            // Read Challenge 2 mark
+            System.out.print("Challenge 2 mark: ");
+            results[i][2] = kb.nextInt();
+
+            // Read Challenge 3 mark
+            System.out.print("Challenge 3 mark: ");
+            results[i][3] = kb.nextInt();
+
+            // Calculate total marks
+            results[i][4] = results[i][1]
+                  + results[i][2]
+                  + results[i][3];
+
+            // Calculate percentage
+            results[i][5] = (results[i][4] / 300.0) * 100;
+
+}
     }
 }
