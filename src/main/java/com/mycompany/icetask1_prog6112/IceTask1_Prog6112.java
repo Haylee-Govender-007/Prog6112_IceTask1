bdb30a3b5779ad2c1cc8bd29fb67982faa9367cc
Haylee-Govender-007 <ST10480667@myemeris.edu.za>
2026-07-30 18:26:54 +0000

    First commit
    copied file

---
+++ src/main/java/com/mycompany/icetask1_prog6112/IceTask1_Prog6112.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.icetask1_prog6112;
+
+/**
+ *
+ * @author emeris
+ */
+public class IceTask1_Prog6112 {
+
+    public static void main(String[] args) {
+     
+    }
+}
