882f9e3b5633ec42834a9b3d1c38eb91dee74bce
Haylee-Govender-007 <ST10480667@myemeris.edu.za>
2026-07-30 18:44:03 +0000

    2nd Commit
    Initial project setup and user input for number of students

--- src/main/java/com/mycompany/icetask1_prog6112/IceTask1_Prog6112.java
+++ src/main/java/com/mycompany/icetask1_prog6112/IceTask1_Prog6112.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.icetask1_prog6112;
 
+import java.util.Scanner;
+
 /**
  *
  * @author emeris
@@ -11,6 +13,12 @@ package com.mycompany.icetask1_prog6112;
 public class IceTask1_Prog6112 {
 
     public static void main(String[] args) {
-     
+        
+        // Import Scanner object for user input
+        Scanner kb = new Scanner(System.in);
+
+        // Prompt the user for how many students there are
+        System.out.print("Enter the number of students: ");
+        int students = kb.nextInt();
     }
 }
